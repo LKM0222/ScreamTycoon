599abb3b2d3f939ebc4df0f561b1a2172a15d955
LKM0222WIN <python03@naver.com>
2024-06-23 10:27:26 +0900

    버튼 스크립트 추가

---
+++ Assets/01.Script/TitleScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : MonoBehaviour
+{
+    public void GameStart(){
+        SceneManager.LoadScene("");
+    }
+
+    public void Credit(){
+
+    }
+
+    public void GameQuit(){
+
+    }
+}

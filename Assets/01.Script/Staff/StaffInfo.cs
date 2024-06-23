d98b2dd80e354361dde4608284a34fbca0d1872c
gorani4989i <82318619+gorani4989i@users.noreply.github.com>
2024-06-23 12:09:03 +0900

    수정

--- Assets/01.Script/Staff/StaffInfo.cs
+++ Assets/01.Script/Staff/StaffInfo.cs
@@ -6,26 +6,26 @@ public class StaffInfo : MonoBehaviour
 {
     public StaffType type;
 
-    private void Start()
-    {
-        switch (type)
-        {
-            case StaffType.Lost:
-                GameManager.Instance.IsLostStaff += ListenStaffAction;
-                break;
-            case StaffType.Enter:
-                GameManager.Instance.EnterStaffAction += ListenStaffAction;
-                break;
-            case StaffType.Doll:
-                GameManager.Instance.DollStaffAction += ListenStaffAction;
-                break;
-        }
-    }
+    //private void Start()
+    //{
+    //    switch (type)
+    //    {
+    //        //case StaffType.Lost:
+    //        //    GameManager.Instance.IsLostStaff += ListenStaffAction;
+    //        //    break;
+    //        case StaffType.Enter:
+    //            GameManager.Instance.EnterStaffAction += ListenStaffAction;
+    //            break;
+    //        case StaffType.Doll:
+    //            GameManager.Instance.DollStaffAction += ListenStaffAction;
+    //            break;
+    //    }
+    //}
 
-    private void ListenStaffAction(bool isOn)
-    {
-        this.gameObject.SetActive(isOn);
-    }
+    //private void ListenStaffAction(bool isOn)
+    //{
+    //    this.gameObject.SetActive(isOn);
+    //}
 }
 
 public enum StaffType
